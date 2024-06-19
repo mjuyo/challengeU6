a486e7cd2ab3dddd1e886bfe06199d64916693a3
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-06-18 20:58:43 -0500

    db migrate, sassc setup

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "email_address", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
+    end
+
+    
 end
