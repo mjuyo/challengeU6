7e39ecd76b1b6f429d80c0a2f7ecd94eba142456
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-06-19 17:18:43 -0500

    Rails setup complete, creation of 5 customers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -15,4 +15,48 @@ ActiveAdmin.register Customer do
   #   permitted
   # end
   
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  # Remove ActiveStorage filters
+  remove_filter :image_attachment, :image_blob
+  # filter :has_one_attached 
+
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email_address
+    column :notes
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Customer Details' do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag url_for(customer.image)
+        end
+      end
+    end
+  end
+
+  
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,13 @@ class Customer < ApplicationRecord
         ["created_at", "email_address", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
     end
 
-    
+    def self.ransackable_associations(auth_object = nil)
+        ["image_attachment", "image_blob"]
+    end
+
+    has_one_attached :image
+
+    validates :full_name, presence: true
+    validates :phone_number, presence: true
 end
+
